18b548c4b91e21d877e6b735f38ba36d7f163342
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:44:31 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "monitor_private_link_scoped_services" {
+  description = "All monitor_private_link_scoped_service resources"
+  value       = azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services
+}
+
