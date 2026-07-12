9158f5e3f34d36b4df687cc92faaffcaff8b52f5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "monitor_private_link_scoped_services" {
-  description = "All monitor_private_link_scoped_service resources"
-  value       = azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services
-}
 output "monitor_private_link_scoped_services_linked_resource_id" {
-  description = "List of linked_resource_id values across all monitor_private_link_scoped_services"
-  value       = [for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : v.linked_resource_id]
+  description = "Map of linked_resource_id values across all monitor_private_link_scoped_services, keyed the same as var.monitor_private_link_scoped_services"
+  value       = { for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : k => v.linked_resource_id }
 }
 output "monitor_private_link_scoped_services_name" {
-  description = "List of name values across all monitor_private_link_scoped_services"
-  value       = [for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : v.name]
+  description = "Map of name values across all monitor_private_link_scoped_services, keyed the same as var.monitor_private_link_scoped_services"
+  value       = { for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : k => v.name }
 }
 output "monitor_private_link_scoped_services_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_private_link_scoped_services"
-  value       = [for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_private_link_scoped_services, keyed the same as var.monitor_private_link_scoped_services"
+  value       = { for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : k => v.resource_group_name }
 }
 output "monitor_private_link_scoped_services_scope_name" {
-  description = "List of scope_name values across all monitor_private_link_scoped_services"
-  value       = [for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : v.scope_name]
+  description = "Map of scope_name values across all monitor_private_link_scoped_services, keyed the same as var.monitor_private_link_scoped_services"
+  value       = { for k, v in azurerm_monitor_private_link_scoped_service.monitor_private_link_scoped_services : k => v.scope_name }
 }
 
